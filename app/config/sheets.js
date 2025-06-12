b512815d0bcd5b2742b0457db82d1a9d0a85c115
Sofia Albuquerque Lima <151922342+SofiaAlbuLima@users.noreply.github.com>
2025-06-12 17:15:43 -0300

    Workspace (#2)
    * 🟢 Arrumar css, quando há muitas atividades (mobile, tablet e desktop!)
    
    * 🟢 Atividade alterar automaticamente conforme as datas limites de atividades
    
    ---------
    
    Co-authored-by: seu-usuario <seu-email@exemplo.com>

--- app/config/sheets.js
+++ app/config/sheets.js
@@ -2,6 +2,11 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 const doc = new GoogleSpreadsheet('14bLyhRhfclC3x9iAqt6JvaeeE3phmikOilTEKE_We2o');
 
+function parseDataBrasil(dataStr) {
+  const [dia, mes, ano] = dataStr.split('/');
+  return new Date(`${ano}-${mes}-${dia}`);
+}
+
 async function getSheetData() {
   await doc.useServiceAccountAuth(require('./service-account.json'));
   await doc.loadInfo();
@@ -9,14 +14,23 @@ async function getSheetData() {
   const sheet = doc.sheetsByTitle['Página1'];
   const rows = await sheet.getRows();
   
-  return rows.map(row => ({
-      ativo: row.ativo,
+  return rows.map(row => {
+    const limiteStr = row.data_pra_sumir;
+    const dataLimite = parseDataBrasil(limiteStr);
+    const hoje = new Date();
+    
+    const ativo = dataLimite >= hoje && row.ativo != 'FALSE';
+
+    return {
+      ativo: ativo,
       tipo: row.tipo,
       nome: row.nome_atividade,
       imagem: row.imagem_url,
       periodo: row.periodo_ou_data,
-      link: row.link 
-    }));
+      link: row.link,
+      limite: limiteStr
+    };
+  });
 }
 
 module.exports = {
